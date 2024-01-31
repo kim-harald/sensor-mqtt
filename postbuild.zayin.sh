597a479714db84cce61eb04f3e1827ff15575230
kim-harald <kim@sticos.no>
2024-01-31 07:41:12 +0100

    no need for ssl cert

--- postbuild.zayin.sh
+++ postbuild.zayin.sh
@@ -3,5 +3,4 @@ rm /home/kim/zayin/home/pi/apps/sensor/config/*
 cp ./dist/. /home/kim/zayin/home/pi/apps/sensor/ -r
 cp ./src/config/default.zayin.json /home/kim/zayin/home/pi/apps/sensor/config/default.json
 rm /home/kim/zayin/home/pi/apps/sensor/config/default.*.json -f
-rm /home/kim/zayin/home/pi/apps/sensor/config/openssl.conf
 cp ./sensor.service /home/kim/zayin/home/pi/apps/sensor
